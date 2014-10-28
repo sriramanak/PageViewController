33f51cc02388a4be45c659ff6b6eda3fea7f9192
paradigm creatives <paradigm@paradigms-Mac-mini.local>
2014-10-28 11:46:37 +0530

    Initial Commit

---
+++ PageView Example/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PageView Example
+//
+//  Created by paradigm creatives on 10/28/14.
+//  Copyright (c) 2014 Paradigm Creatives. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PageView Example/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PageView Example
+//
+//  Created by paradigm creatives on 10/28/14.
+//  Copyright (c) 2014 Paradigm Creatives. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
